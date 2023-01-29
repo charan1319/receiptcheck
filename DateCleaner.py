3609fcb51c2fc90b833641ece963f7f3f761c575
amanshanbhag <amanshanbhag@gmail.com>
2023-01-28 19:05:20 -0600

    Fixing length of date array

--- DateCleaner.py
+++ DateCleaner.py
@@ -62,43 +62,49 @@ def get_reformatted_strings():
     dir = 'Bill.com/ocr/'
 
     parsed_ocr_dict = ocr_parser.parse_files()
+    # print("LENGTH", len(parsed_ocr_dict))
     # document_id_list = []
     mapping = dict()
-    for name in os.listdir(dir):
-        if name.endswith(".csv"):
-            converted_string_list = []
-            stripped_name = name.replace('.csv','')
-            OCR_CSV = parsed_ocr_dict[stripped_name]
-            # OCR_CSV = pd.read_csv(dir + '/' + name, names=["x1","y1","x2","y2","x3","y3","x4","y4","TEXT"], engine='python', on_bad_lines='skip')
+    # for name in os.listdir(dir):
+    for name in parsed_ocr_dict.keys():
+        # if name.endswith(".csv"):
+        converted_string_list = []
+        # stripped_name = name.replace('.csv','')
+        OCR_CSV = parsed_ocr_dict[name]
+        # OCR_CSV = pd.read_csv(dir + '/' + name, names=["x1","y1","x2","y2","x3","y3","x4","y4","TEXT"], engine='python', on_bad_lines='skip')
 
-            OCR_CSV_STRING = OCR_CSV.to_string()
-            numeric_re = '\d{2}[,\s/-]\d{2}[,\s/-]\d+'
-            alphanumeric_re_1 = '\s(Jan|JAN|Feb|FEB|Mar|MAR|Apr|APR|May|MAY|Jun|JUN|Jul|JUL|Aug|AUG|Sep|SEP|Oct|OCT|Nov|NOV|Dec|DEC)[a-zA-Z.,-]*[,\s/-]?(\d{1,2})?[,\s/-]?\d+'
-            alphanumeric_re_2 = '\s(\d{1,2})?[,\s/-]?(Jan|JAN|Feb|FEB|Mar|MAR|Apr|APR|May|MAY|Jun|JUN|Jul|JUL|Aug|AUG|Sep|SEP|Oct|OCT|Nov|NOV|Dec|DEC)[a-zA-Z.,-]*[,\s/-]?\d+'
-            reg = numeric_re + '|' + alphanumeric_re_1 + '|' + alphanumeric_re_2
+        OCR_CSV_STRING = OCR_CSV.to_string()
+        numeric_re = '\d{2}[,\s/-]\d{2}[,\s/-]\d+'
+        alphanumeric_re_1 = '\s(Jan|JAN|Feb|FEB|Mar|MAR|Apr|APR|May|MAY|Jun|JUN|Jul|JUL|Aug|AUG|Sep|SEP|Oct|OCT|Nov|NOV|Dec|DEC)[a-zA-Z.,-]*[,\s/-]?(\d{1,2})?[,\s/-]?\d+'
+        alphanumeric_re_2 = '\s(\d{1,2})?[,\s/-]?(Jan|JAN|Feb|FEB|Mar|MAR|Apr|APR|May|MAY|Jun|JUN|Jul|JUL|Aug|AUG|Sep|SEP|Oct|OCT|Nov|NOV|Dec|DEC)[a-zA-Z.,-]*[,\s/-]?\d+'
+        reg = numeric_re + '|' + alphanumeric_re_1 + '|' + alphanumeric_re_2
 
-            dates = re.search(reg, OCR_CSV_STRING)
-            # print("MAIN EXTRACTED DATE", dates)
-            if dates:
-                date_str = dates.group(0)
-                # print("FILE", name)
-                # print("EXTRACTED DATE", dates.group(0))
-                # print("ENTERING FUNCTION")
-                current_format = get_current_format(date_str, current_formats)
-                # print("EXITING FUNCTION")
-                # print("CURRENT FORMAT", current_format)
-                # YYYY-MM-DD
-                converted_string_1 = convert_to_format(date_str, current_format, required_format_1)
-                # YYYY-DD-MM
-                converted_string_2 = convert_to_format(date_str, current_format, required_format_2)
-                # print("CONVERTED DATE", converted_string)
-                # print(converted_string)
-                # document_id_list.append(stripped_name)
-                converted_string_list.append(converted_string_1)
-                converted_string_list.append(converted_string_2)
-                mapping[stripped_name] = converted_string_list
+        dates = re.search(reg, OCR_CSV_STRING)
+        # print("MAIN EXTRACTED DATE", dates)
+        if dates:
+            date_str = dates.group(0)
+            # print("FILE", name)
+            # print("EXTRACTED DATE", dates.group(0))
+            # print("ENTERING FUNCTION")
+            current_format = get_current_format(date_str, current_formats)
+            # print("EXITING FUNCTION")
+            # print("CURRENT FORMAT", current_format)
+            # YYYY-MM-DD
+            converted_string_1 = convert_to_format(date_str, current_format, required_format_1)
+            # YYYY-DD-MM
+            converted_string_2 = convert_to_format(date_str, current_format, required_format_2)
+            # print("CONVERTED DATE", converted_string)
+            # print(converted_string)
+            # document_id_list.append(stripped_name)
+            converted_string_list.append(converted_string_1)
+            converted_string_list.append(converted_string_2)
+            mapping[name] = converted_string_list
+        else:
+            date_str = None
+            mapping[name] = date_str    
     # print(mapping)
-    # return document_id_list, converted_string_list                
+    # return document_id_list, converted_string_list
+    # print(len(mapping))                
     return mapping
 
 # print(get_reformatted_strings())
